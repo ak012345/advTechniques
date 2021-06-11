87880003e4510e81394426606ec91eda657182d8
akernan2 <akernan2@cs6261-vm08.uwg.westga.edu>
2021-06-11 14:30:23 -0400

    Fixed part 1 navigation. typo in main component html

--- project2/src/app/app-routing.module.ts
+++ project2/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegistrationComponent } from './tournament/registration/registration.co
 import { BracketsComponent } from './tournament/brackets/brackets.component'
 
 const routes: Routes = [
+{path: '',  redirectTo: '/hello', pathMatch: 'full'},
 {path: 'hello' , component: HelloComponent },
 {path: 'registration', component: RegistrationComponent},
 {path: 'brackets', component: BracketsComponent },
