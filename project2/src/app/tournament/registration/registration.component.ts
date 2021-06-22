9bba5fcd04557a1722048f9e22a116bbcd02a682
Alex Kernan <akernan2@westga.edu>
2021-06-21 20:14:29 -0400

    saving progress

--- project2/src/app/tournament/registration/registration.component.ts
+++ project2/src/app/tournament/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RosterService } from '../../services/roster.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  public players: string[];
+  public informationMessages: string;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.players = ['', '', '', '', '', '', '', ''];
+    this.informationMessages = '';
+  }
+
+  registerContestants(){
+    for(let current of this.players){
+
+    }
+  }
+
+  ngOnInit(): void{
   }
 
 }
