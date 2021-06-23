b25e320c1c38f9673e3590757326f254710429d2
Alex Kernan <akernan2@my.westga.edu>
2021-06-23 09:53:35 -0400

    still crashing. considering this could be caused by an non-async read/write.

--- project2/src/app/tournament/brackets/brackets.component.ts
+++ project2/src/app/tournament/brackets/brackets.component.ts
@@ -11,22 +11,23 @@ import { Match } from 'src/app/model/match';
 export class BracketsComponent  {
   public informationMessage: string;
   public roundNumber: number = 1
-  public matchesArray: Match[] = []
+  public matchesArray: Promise<Match[]>;
 
   constructor(public playerRoster: RosterService) {
-
+    this.matchesArray = BracketsComponent.makeMatches(this.playerRoster.getContestants()).then(result => {
+     return result;
+     });
    
   }
 
   ngOnInit(): void {
-    this.matchesArray = BracketsComponent.makeMatches(this.playerRoster.getContestants());
   }
 
   completeRound(){
     this.roundNumber++;
   }
 
-static makeMatches(players: string[]): Match[]{
+ static async makeMatches(players: string[]):Promise<Match[]>{
     let localMatchArray: Match[] = [];
     for(let index = 0; index < players.length/2; index+2){
       let currentMatch = new Match(players[index],players[index+1])
