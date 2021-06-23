1da0295d46dd803622818f5e5426dc3851de67aa
Alex Kernan <akernan2@my.westga.edu>
2021-06-23 11:59:39 -0400

    wrote tests for brackets component

--- project2/src/app/tournament/brackets/brackets.component.spec.ts
+++ project2/src/app/tournament/brackets/brackets.component.spec.ts
@@ -22,4 +22,37 @@ describe('BracketsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('makeMatches', () => {
+   
+    it('should return 2 matches', () => {
+      let players = ['Anorak', 'Parzival', 'Aech', 'Art3mis']
+      let matches = component.makeMatches(players);
+      expect(matches.length).toEqual(2)
+    })
+
+    it('should return Anorak', () => {
+      let players = ['Anorak', 'Parzival', 'Aech', 'Art3mis']
+      let matches = component.makeMatches(players);
+      expect(matches[0].player1).toEqual('Anorak')
+    })
+   
+    it('should return Parzival', () => {
+      let players = ['Anorak', 'Parzival', 'Aech', 'Art3mis']
+      let matches = component.makeMatches(players);
+      expect(matches[0].player2).toEqual('Parzival')
+    })
+
+    it('should return Aech', () => {
+      let players = ['Anorak', 'Parzival', 'Aech', 'Art3mis']
+      let matches = component.makeMatches(players);
+      expect(matches[1].player1).toEqual('Aech')
+    })
+
+    it('should return Art3mis', () => {
+      let players = ['Anorak', 'Parzival', 'Aech', 'Art3mis']
+      let matches = component.makeMatches(players);
+      expect(matches[1].player2).toEqual('Art3mis')
+    })
+  });
 });
