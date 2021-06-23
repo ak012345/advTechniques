45ff29a8f928d5427668f5f0fbf81e40f1f86b1a
Alex Kernan <akernan2@my.westga.edu>
2021-06-23 01:15:22 -0400

    added tests for registration

--- project2/src/app/tournament/registration/registration.component.spec.ts
+++ project2/src/app/tournament/registration/registration.component.spec.ts
@@ -1,16 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NgForm, NgModel } from '@angular/forms'
+import { RosterService } from 'src/app/services/roster.service'
 import { RegistrationComponent } from './registration.component';
 
-describe('RegistrationComponent', () => {
+describe('RegistrationComponent with REAL SERVICE', () => {
   let component: RegistrationComponent;
   let fixture: ComponentFixture<RegistrationComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegistrationComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        RegistrationComponent,
+        NgModel,
+        NgForm
+      ],
+      providers: [RosterService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +27,43 @@ describe('RegistrationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have all players equal to an empty string', () => {
+    component.players.forEach((player: string) => expect(player).toEqual(''))
+  })
+
+  describe('registerContestants', () => {
+    it('all players should be empty', () => {
+      component.registerContestants()
+      expect(component.playerRoster.getContestants()).toEqual([])
+    })
+
+    it('roster should not be allowed to have duplicate names', () => {
+      component.players = ['Anorak', 'Parzival', 'Aech', 'Anorak']
+      component.registerContestants()
+      expect(component.playerRoster.getContestants()).toEqual([])
+    })
+
+    it('roster has 2 players', () => {
+      component.players = ['Anorak', 'Parzival']
+      component.registerContestants()
+      expect(component.playerRoster.getContestants()).toEqual(['Anorak', 'Parzival'])
+    })
+
+    it('roster has 4 players', () => {
+      component.players = ['Anorak', 'Parzival', 'Ach', 'Art3mis']
+      component.registerContestants()
+      expect(component.playerRoster.getContestants()).toEqual(['Anorak', 'Parzival', 'Aech', 'Art3mis'])
+    })
+
+    it('roster has 8 players', () => {
+      component.players = ['Anorak', 'Parzival', 'Aech', 'Art3mis', 'Sho','Daito','TGAP_Ogg','iRok']
+      component.registerContestants()
+      expect(component.playerRoster.getContestants()).toEqual(['Anorak', 'Parzival', 'Aech', 'Art3mis', 'Sho','Daito','TGAP_Ogg','iRok'])
+    })
+
+  })
 });
+
+// Make sure you cover cases like duplicates, 
+// invalid numbers of players, "empty" players between valid players, 
